021ac51548e872ac32d3a49dc7a859fe85602bd8
Austeja <sketeryteausteja@gmail.com>
2024-10-30 13:25:31 +0100

    Add popup image saving to Firestore

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -5,225 +5,234 @@ import 'leaflet-contextmenu/dist/leaflet.contextmenu.css'
 import 'leaflet-contextmenu'
 import MarkerComponent from './MarkerComponent'
 import { doc, setDoc, getDoc } from "firebase/firestore"
+import { getStorage, ref, deleteObject } from "firebase/storage"
 
 
-const MapComponent = ({ selectedColor, addedCountry, selectedCountries, onAddCountry, onRemoveCountry, getAllCountries, currentUser, db }) => {
-    const center = [0, 0]
-    const maxBounds = [[-90, -180], [90, 180]]
-    const [geoJSONData, setGeoJSONData] = useState(null)
-    const geoJsonLayerRef = useRef()
-    const selectedColorRef = useRef(selectedColor)
-    const [markers, setMarkers] = useState([])
+const MapComponent = ({
+  selectedColor,
+  addedCountry,
+  selectedCountries,
+  onAddCountry,
+  onRemoveCountry,
+  getAllCountries,
+  currentUser,
+  db
+}) => {
 
-    const customRenderer = L.svg({ padding: 0.4 });
+  const center = [0, 0]
+  const maxBounds = [[-90, -180], [90, 180]]
+  const [geoJSONData, setGeoJSONData] = useState(null)
+  const geoJsonLayerRef = useRef()
+  const customRenderer = L.svg({ padding: 0.4 });
+  const storage = getStorage()
+  const [markers, setMarkers] = useState([])
 
-
-    // Fetching countries from geoJson and saving them in a state. Calling getAllCountries with all country names from geoJson.
-
-    useEffect(() => {
-      fetch('/countries.geojson')
-        .then(res => res.json())
-        .then(data => {
-          setGeoJSONData(data)
-          const countires = data.features.map(feature => feature.properties.ADMIN.toLowerCase())
-          getAllCountries(countires)
-        })
-        .catch(error => console.error('Error fetching GeoJSON:', error))
-    }, [])
-
-    useEffect(() => {
-      const loadMarkers = async () => {
-        if (currentUser) {
-          const userDocRef = doc(db, 'users', currentUser.uid)
-          const userDoc = await getDoc(userDocRef)
-          if (userDoc.exists()) {
-            const data = userDoc.data()
-            setMarkers(data.markers || [])
-          }
-        }
-      }
-      loadMarkers()
-    }, [currentUser, db])
-
-    useEffect(() => {
-      if (addedCountry) {
-        if (!selectedCountries.includes(addedCountry)) {
-          onAddCountry(addedCountry)
-        }
-      }
-    }, [addedCountry])
-
-
-    const clickTimeout = useRef(null)
-
-    const onEachCountry = (country, layer) => {
-
-      layer.bindTooltip(country.properties.ADMIN, {
-        direction: 'auto',
-        offset: [0,0],
-        className: 'custom-tooltip'
+  useEffect(() => {
+    fetch('/countries.geojson')
+      .then(res => res.json())
+      .then(data => {
+        setGeoJSONData(data)
+        const countires = data.features.map(feature => feature.properties.ADMIN.toLowerCase())
+        getAllCountries(countires)
       })
-
-      const countryName = country.properties.ADMIN.toLowerCase()
-
-      layer.on({
-
-        click: () => {
-          if (clickTimeout.current) {
-            clearTimeout(clickTimeout.current)
-            clickTimeout.current = null
-          } else { 
-            clickTimeout.current = setTimeout(() => {
-              clickTimeout.current = null
-              if (!selectedCountries.includes(countryName)) {
-                onAddCountry(countryName)
-              }
-            }, 200)
-          }
-        },
-
-        dblclick: () => {
-          if (clickTimeout.current) {
-            clearTimeout(clickTimeout.current)
-            clickTimeout.current = null
-          }
-          onRemoveCountry(countryName)
-        }
-      })
-    }
+      .catch(error => console.error('Error fetching GeoJSON:', error))
+  }, [])
 
 
-    const defaultStyle = {
-      fillColor: '#FFFFFF',
-      color: 'none',
-      fillOpacity: 0.5
-    }
-
-    const geoJsonStyle = (feature) => {
-      const countryName = feature.properties.ADMIN.toLowerCase()
-
-      if (selectedCountries.includes(countryName)) {
-        return {
-          fillColor: selectedColor || '#348285',
-          color: 'none',
-          fillOpacity: 0.5,
-          weight: 1
-        }
-      }
-      return defaultStyle
-    }
-
-
-    const addMarker = (e, catogory) => {
-      const newMarker = {
-        id: Date.now(),
-        position: [e.latlng.lat, e.latlng.lng],
-        title: '',
-        image: null,
-        isEditing: true,
-        category: catogory
-      }
-      setMarkers(prevMarkers => {
-        const updatedMarkers = [...prevMarkers, newMarker]
-        saveMarkersToFirestore(updatedMarkers)
-        return updatedMarkers
-      })
-    }
-
-    const removeMarker = (markerId) => {
-      setMarkers(prevMarkers => {
-        const updatedMarkers = prevMarkers.filter(marker => markerId !== marker.id)
-        saveMarkersToFirestore(updatedMarkers)
-        return updatedMarkers
-      })
-    }
-
-    const handleMarkerTitleChange = (markerId, newTitle) => {
-      setMarkers(prevMarkers => {
-        const updatedMarkers = prevMarkers.map(marker =>
-          marker.id === markerId ? { ...marker, title: newTitle } : marker
-        )
-        saveMarkersToFirestore(updatedMarkers)
-        return updatedMarkers
-      })
-    }
-
-    const handleMarkerSubmit = (markerId) => {
-      setMarkers(prevMarkers => {
-        const updatedMarkers = prevMarkers.map(marker => 
-          marker.id === markerId ? { ...marker, isEditing: false } : marker
-        )
-        saveMarkersToFirestore(updatedMarkers)
-        return updatedMarkers
-      })
-    }
-
-    const saveMarkersToFirestore = async (markers) => {
+  useEffect(() => {
+    const loadMarkers = async () => {
       if (currentUser) {
-        try {
-          const userDocRef = doc(db, 'users', currentUser.uid)
-          await setDoc(userDocRef, { markers: markers }, { merge: true })
-        } catch (error) {
-          console.error('Error saving markers: ', error)
+        const userDocRef = doc(db, 'users', currentUser.uid)
+        const userDoc = await getDoc(userDocRef)
+        if (userDoc.exists()) {
+          const data = userDoc.data()
+          setMarkers(data.markers || [])
         }
       }
     }
-    
-    return (
-        <MapContainer 
-            center={center}
-            zoom={2}
-            minZoom={2}
-            maxZoom={18}
-            className='map'
-            zoomControl={false}
-            doubleClickZoom={false}
-            maxBounds={maxBounds}
-            maxBoundsViscosity={1.0}
-            contextmenu={true}
-            contextmenuWidth={140}
-            contextmenuItems= {[
-              {text: '<strong>Choose Category</strong>',
-              callback: null,
-              disabled: true},
-              {text: `<i class="fa-solid fa-hotel"></i> Hotel`,
-              callback: (e) => addMarker(e, 'hotel')},
-              {text: `<i class="fas fa-utensils"></i> Restaurant`,
-              callback: (e) => addMarker(e, 'restaurant')},
-              {text: `<i class="fas fa-landmark"></i> Museum`,
-              callback: (e) => addMarker(e, 'museum')},
-              {text: `<i class="fa-solid fa-monument"></i> Landmark`,
-              callback: (e) => addMarker(e, 'landmark')},
-              {text: `<i class="fa-solid fa-person-hiking"></i> Hiking Spot`,
-              callback: (e) => addMarker(e, 'hiking')},
-              {text: `<i class="fa-solid fa-location-pin"></i> Other`,
-              callback: (e) => addMarker(e, 'other')}
-          ]}
-        >
-            <TileLayer
-                url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            />
-            {geoJSONData && (
-                <GeoJSON 
-                  data={geoJSONData}
-                  onEachFeature={onEachCountry}
-                  ref={geoJsonLayerRef}
-                  style={geoJsonStyle}
-                  renderer={customRenderer}
-                />
-            )}
-            {markers && markers.length > 0 && markers.map((marker) => (
-              <MarkerComponent
-                marker={marker}
-                setMarkers={setMarkers}
-                onRemoveMarker={removeMarker}
-                onMarkerTitleChange={handleMarkerTitleChange}
-                onMarkerSubmit={handleMarkerSubmit}
+    loadMarkers()
+  }, [currentUser, db])
+
+
+  useEffect(() => {
+    if (addedCountry) {
+      if (!selectedCountries.includes(addedCountry)) {
+        onAddCountry(addedCountry)
+      }
+    }
+  }, [addedCountry])
+
+
+  const clickTimeout = useRef(null)
+
+  const onEachCountry = (country, layer) => {
+
+    layer.bindTooltip(country.properties.ADMIN, {
+      direction: 'auto',
+      offset: [0,0],
+      className: 'custom-tooltip'
+    })
+
+    const countryName = country.properties.ADMIN.toLowerCase()
+
+    layer.on({
+
+      click: () => {
+        if (clickTimeout.current) {
+          clearTimeout(clickTimeout.current)
+          clickTimeout.current = null
+        } else { 
+          clickTimeout.current = setTimeout(() => {
+            clickTimeout.current = null
+            if (!selectedCountries.includes(countryName)) {
+              onAddCountry(countryName)
+            }
+          }, 200)
+        }
+      },
+
+      dblclick: () => {
+        if (clickTimeout.current) {
+          clearTimeout(clickTimeout.current)
+          clickTimeout.current = null
+        }
+        onRemoveCountry(countryName)
+      }
+    })
+  }
+
+
+  const defaultStyle = {
+    fillColor: '#FFFFFF',
+    color: 'none',
+    fillOpacity: 0.5
+  }
+
+  const geoJsonStyle = (feature) => {
+    const countryName = feature.properties.ADMIN.toLowerCase()
+
+    if (selectedCountries.includes(countryName)) {
+      return {
+        fillColor: selectedColor || '#348285',
+        color: 'none',
+        fillOpacity: 0.5,
+        weight: 1
+      }
+    }
+    return defaultStyle
+  }
+
+
+  const addMarker = (e, catogory) => {
+    const newMarker = {
+      id: Date.now(),
+      position: [e.latlng.lat, e.latlng.lng],
+      title: '',
+      image: null,
+      isEditing: true,
+      category: catogory
+    }
+    setMarkers(prevMarkers => {
+      const updatedMarkers = [...prevMarkers, newMarker]
+      saveMarkersToFirestore(updatedMarkers)
+      return updatedMarkers
+    })
+  }
+
+  const removeMarker = (markerId) => {
+    setMarkers(prevMarkers => {
+      const selectedMarker = prevMarkers.find(marker => markerId === marker.id)
+
+      if (selectedMarker.imagePath) {
+        const imageRef = ref(storage, selectedMarker.imagePath)
+        deleteObject(imageRef).catch(error => {
+          console.error('Error deleting image: ', error)
+        })
+      }
+
+      const updatedMarkers = prevMarkers.filter(marker => markerId !== marker.id)
+      saveMarkersToFirestore(updatedMarkers)
+      return updatedMarkers
+    })
+  }
+
+  const handleMarkerSubmit = (markerId, markerData) => {
+    setMarkers(prevMarkers => {
+      const updatedMarkers = prevMarkers.map(marker => 
+        marker.id === markerId ? { ...marker, ...markerData } : marker
+      )
+      saveMarkersToFirestore(updatedMarkers)
+      return updatedMarkers
+    })
+  }
+
+  const saveMarkersToFirestore = async (markers) => {
+    if (currentUser) {
+      try {
+        const userDocRef = doc(db, 'users', currentUser.uid)
+        await setDoc(userDocRef, { markers: markers }, { merge: true })
+      } catch (error) {
+        console.error('Error saving markers: ', error)
+      }
+    }
+  }
+  
+  return (
+      <MapContainer 
+          center={center}
+          zoom={2}
+          minZoom={2}
+          maxZoom={18}
+          className='map'
+          zoomControl={false}
+          doubleClickZoom={false}
+          maxBounds={maxBounds}
+          maxBoundsViscosity={1.0}
+          contextmenu={true}
+          contextmenuWidth={140}
+          contextmenuItems= {[
+            {text: '<strong>Choose Category</strong>',
+            callback: null,
+            disabled: true},
+            {text: `<i class="fa-solid fa-hotel"></i> Hotel`,
+            callback: (e) => addMarker(e, 'hotel')},
+            {text: `<i class="fas fa-utensils"></i> Restaurant`,
+            callback: (e) => addMarker(e, 'restaurant')},
+            {text: `<i class="fas fa-landmark"></i> Museum`,
+            callback: (e) => addMarker(e, 'museum')},
+            {text: `<i class="fa-solid fa-monument"></i> Landmark`,
+            callback: (e) => addMarker(e, 'landmark')},
+            {text: `<i class="fa-solid fa-person-hiking"></i> Hiking Spot`,
+            callback: (e) => addMarker(e, 'hiking')},
+            {text: `<i class="fa-solid fa-location-pin"></i> Other`,
+            callback: (e) => addMarker(e, 'other')}
+        ]}
+      >
+          <TileLayer
+              url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          {geoJSONData && (
+              <GeoJSON 
+                data={geoJSONData}
+                onEachFeature={onEachCountry}
+                ref={geoJsonLayerRef}
+                style={geoJsonStyle}
+                renderer={customRenderer}
               />
-            ))}
-        </MapContainer>
-    )
+          )}
+          {markers && markers.length > 0 && markers.map(marker => (
+            <MarkerComponent
+              key={marker.id}
+              marker={marker}
+              setMarkers={setMarkers}
+              onRemoveMarker={removeMarker}
+              onMarkerSubmit={handleMarkerSubmit}
+              currentUser={currentUser}
+            />
+          ))}
+      </MapContainer>
+  )
 }
 
 export default MapComponent
--- src/components/MarkerComponent.jsx
+++ src/components/MarkerComponent.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Marker, Popup } from 'react-leaflet'
+import { getStorage, ref, getDownloadURL, uploadBytes } from 'firebase/storage'
 
 import pinIcon from '../assets/pin-icon.png'
 import hotelPin from '../assets/hotel-pin.png'
@@ -7,74 +8,71 @@ import restaurantPin from '../assets/restaurant-pin.png'
 import museumPin from '../assets/museum-pin.png'
 import landmarkPin from '../assets/landmark-pin.png'
 import hikingPin from '../assets/hiking-pin.png'
-
 import uploadIcon from '../assets/upload-icon.png'
 
-const MarkerComponent = ({ marker, setMarkers, onRemoveMarker, onMarkerTitleChange, onMarkerSubmit }) => {
+const MarkerComponent = ({ marker, setMarkers, onRemoveMarker, onMarkerSubmit, currentUser }) => {
+
+    const storage = getStorage()
 
     const markerIcon = () => {
-        if (marker.category === 'hotel') {
-            return L.icon({
-                iconUrl: hotelPin,
-                iconSize: [40, 40]
-            })
+        const iconMap = {
+            hotel: { iconUrl: hotelPin, iconSize: [40, 40] },
+            restaurant: { iconUrl: restaurantPin, iconSize: [35, 35] },
+            museum: { iconUrl: museumPin, iconSize: [40, 40] },
+            landmark: { iconUrl: landmarkPin, iconSize: [40, 40] },
+            hiking: { iconUrl: hikingPin, iconSize: [40, 40] },
+            other: { iconUrl: pinIcon, iconSize: [35, 35] }
         }
-
-        if (marker.category === 'restaurant') {
-            return L.icon({
-                iconUrl: restaurantPin,
-                iconSize: [35, 35]
-            })
-        }
-
-        if (marker.category === 'museum') {
-            return L.icon({
-                iconUrl: museumPin,
-                iconSize: [40, 40]
-            })
-        }
-
-        if (marker.category === 'landmark') {
-            return L.icon({
-                iconUrl: landmarkPin,
-                iconSize: [40, 40]
-            })
-        }
-
-        if (marker.category === 'hiking') {
-            return L.icon({
-                iconUrl: hikingPin,
-                iconSize: [40, 40]
-            })
-        }
-
-        if (marker.category === 'other') {
-            return L.icon({
-                iconUrl: pinIcon,
-                iconSize: [35, 35]
-            })
-        }
+    
+        const { iconUrl, iconSize } = iconMap[marker.category] || iconMap.other;
+        return L.icon({ iconUrl, iconSize })
     }
 
     const handleInputChange = (e, id) => {
         setMarkers(prevMarkers => 
             prevMarkers.map(marker =>
-            marker.id === id ? {...marker, title: e.target.value} : marker
+                marker.id === id ? { ...marker, title: e.target.value } : marker
             )
         )
     }
 
     const handleImageInput = (e, id) => {
-    setMarkers(prevMarkers => 
-        prevMarkers.map(marker =>
-        marker.id === id ? {...marker, image: e.target.files[0]} : marker
-        )
-    )
+        const addedFile = e.target.files[0]
+        if (addedFile) {
+            setMarkers(prevMarkers => 
+                prevMarkers.map(marker =>
+                    marker.id === id ? { ...marker, image: {file: addedFile, name: addedFile.name} } : marker
+                )
+            )
+        }
     }
 
-    const handlePopupSubmit = (e, id) => {
-        e.stopPropagation()
-        onMarkerSubmit(id)
+    const handlePopupSubmit = async (e, id) => {
+        e.preventDefault()
+
+        const imageData = marker.image ? await uploadImage(marker.image.file) : null
+
+        const markerData = {
+            title: marker.title,
+            image: marker.image ? imageData.url : null,
+            imagePath: imageData ? imageData.path : null,
+            isEditing: false
+        }
+
+        onMarkerSubmit(id, markerData)
+    }
+
+    const uploadImage = async (imageFile) => {
+        try {
+            const imagePath = `images/${currentUser.uid}/${imageFile.name}`
+            const storageRef = ref(storage, imagePath)
+            await uploadBytes(storageRef, imageFile)
+            const downloadURL = await getDownloadURL(storageRef)
+            return { url: downloadURL, path: imagePath }
+        } catch (error) {
+            console.error('Error uploading image: ', error)
+            return null
+        }
     }
 
     const removeMarker = (id) => {
@@ -93,33 +91,36 @@ const MarkerComponent = ({ marker, setMarkers, onRemoveMarker, onMarkerTitleChan
                 <div className='popup-inputs-container'>
                     <label htmlFor='title-input' className='popup-label'>Add a title</label>
                     <input
-                    className='title-input'
-                    id='title-input'
-                    type='text'
-                    placeholder='Epic View Hike'
-                    value={marker.title}
-                    onChange={(e) => handleInputChange(e, marker.id)}
+                        className='title-input'
+                        id='title-input'
+                        type='text'
+                        placeholder='Epic View Hike'
+                        value={marker.title}
+                        onChange={(e) => handleInputChange(e, marker.id)}
                     />
                     <label htmlFor='image-import' className='popup-label'>Add an image</label>
                     <input
-                    type='file'
-                    id='image-import'
-                    accept="image/png, image/jpeg"
-                    onChange={(e) => handleImageInput(e, marker.id)}
+                        type='file'
+                        id='image-import'
+                        accept="image/png, image/jpeg"
+                        onChange={(e) => handleImageInput(e, marker.id)}
                     />
-                    {marker.image ? <label htmlFor='image-import' className='image-import-btn'>{marker.image.name}</label> : (
-                        <label htmlFor='image-import' className='image-import-btn'><img className='upload-icon' src={uploadIcon}/>Choose a file...</label>
-                    )
-                    }
+                    {marker.image ? (
+                        <label htmlFor='image-import' className='image-import-btn'>{marker.image.name}</label>
+                    ) : (
+                        <label htmlFor='image-import' className='image-import-btn'>
+                            <img className='upload-icon' src={uploadIcon}/>Choose a file...
+                        </label>
+                    )}
                     <div className='popup-btn-container'>
-                    <button className='popup-submit-btn' onClick={(e) => handlePopupSubmit(e, marker.id)}>Submit</button>
-                    <button className='remove-marker-btn' onClick={() => removeMarker(marker.id)}>Remove Pin</button>
+                        <button className='popup-submit-btn' onClick={(e) => handlePopupSubmit(e, marker.id)}>Submit</button>
+                        <button className='remove-marker-btn' onClick={() => removeMarker(marker.id)}>Remove Pin</button>
                     </div>
                 </div>
                 ) : (
                 <div>
                     <h3>{marker.title || 'No title'}</h3>
-                    {marker.image && <img className='popup-img' src={URL.createObjectURL(marker.image)} />}
+                    {marker.image && <img className='popup-img' src={marker.image} />}
                     <button className='remove-marker-btn' onClick={() => removeMarker(marker.id)}>Remove Pin</button>
                 </div>
                 )}
